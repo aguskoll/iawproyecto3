f33d596d5a0cfeeceb6aae94619cd09b4d7ba2fa
aguskoll <aguskoll@gmail.com>
2016-06-04 12:04:19 -0300

    algunos cambios en las carpetas

--- app/app.js
+++ app/app.js
@@ -12,43 +12,62 @@ config(['$locationProvider', '$routeProvider', function($locationProvider, $rout
 
   $routeProvider.otherwise({redirectTo: '/view1'});
 }]);*/
+'use strict';
 (function(){
     //Variables de ambiente
-    var __env = {};
-    if(window){
-        Object.assign(__env, window.__env);
-    }
-
-    var app = angular.module('library', [ ]);
-    app.constant('__env',__env);
-
-    app.controller('MoviesController', ['$http', '$log', '__env', function($http,$log,__env){
-
-        var pelis = this;
-        this.peliculas =  [ ];
-        this.seleccionada = 0;
-
-        $http({
-            method: 'GET',
-            url: __env.apiUrl + '/api/movies'
-        }).then(function successCallback(response) {
-            $log.log(response.data);
-            pelis.peliculas = response.data;
-        }, function errorCallback(response) {
-            // called asynchronously if an error occurs
-            // or server returns response with an error status.
-        });
-
-        this.selectFilm = function (seleccionada) {
-            this.seleccionada = seleccionada;
-        };
-
-    }]);
-
-    app.controller('SelectionController',function(){
-
-    });
+    angular.module('library', ['ngRoute'])
+        var __env = {};
+        if(window){
+            Object.assign(__env, window.__env);
+        }
+        constant('__env',__env)
 
 
+
+        .config(['$routeProvider', function($routeProvider) {
+            $routeProvider
+
+                .when('/crearPelicula', {
+                    templateUrl: 'vistas/paginas/peliculas/crear.html'
+                })
+                .when('/eliminarPelicula', {
+                    templateUrl: 'vistas/paginas/peliculas/eliminar.html'
+
+                })
+                .otherwise({
+                        redirectTo:'/index.html'
+                    }
+                )
+        }])
+
+            .controller('MoviesController', ['$http', '$log', '__env', function($http,$log,__env){
+
+            var pelis = this;
+            this.peliculas =  [ ];
+            this.seleccionada = 0;
+
+            $http({
+                method: 'GET',
+                url: __env.apiUrl + '/api/movies'
+            }).then(function successCallback(response) {
+                $log.log(response.data);
+                pelis.peliculas = response.data;
+            }, function errorCallback(response) {
+                // called asynchronously if an error occurs
+                // or server returns response with an error status.
+            });
+
+            this.selectFilm = function (seleccionada) {
+                this.seleccionada = seleccionada;
+            };
+
+    }])
+
+    .controller('SelectionController',function(){
+
+    })
+
+
+    
 })();
 
---
+++ app/js/peliculas/crear.js
@@ -0,0 +1,20 @@
+
+  'use strict';
+
+  angular.module('myApp.crear', [])
+    /*
+      .config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.when('/view1', {
+          templateUrl: 'view1/crear.html',
+          controller: 'CrearPeliculasCtrl'
+        });
+      }])
+    */
+      .controller('CrearPeliculas', [function () {
+
+
+          this.addPelicula = function(pelicula){
+
+          };
+
+      }]);
