a4535372fd6c8453eb077d7f31fcb00f88614309
aguskoll <aguskoll@gmail.com>
2016-06-08 20:06:32 -0300

    anda el autocompletar y el crear

--- app/app.js
+++ app/app.js
@@ -110,12 +110,17 @@
             }).then(function successCallback(response) {
                 $log.log(response.data.Title);
                 $scope.peliculaReferencia=response.data;
+                crear.pelicula.sinopsis=response.data.Plot;
+                crear.pelicula.directores=response.data.Director;
+                crear.pelicula.actores=response.data.Actors;
+                crear.pelicula.duracion=response.data.Runtime;
+                crear.pelicula.fecha=response.data.Year;
             }, function errorCallback(response) {
                 $log.log(response);
             });
 
         };
-        $scope.status = '  ';
+
 
         $scope.showAlert = function(ev) {
             window.alert("pelicula creada");
@@ -164,5 +169,129 @@
             return out;
         }
     });
+    //---face
+    app.run(['$rootScope', '$window',
+        function($rootScope, $window) {
+
+            $rootScope.user = {};
+
+            $window.fbAsyncInit = function() {
+                // Executed when the SDK is loaded
+
+                FB.init({
+
+                    appId: '501245940060729',
+
+                    /*
+                     Adding a Channel File improves the performance
+                     of the javascript SDK, by addressing issues
+                     with cross-domain communication in certain browsers.
+                     */
+
+                    channelUrl: 'app/channel.html',
+
+                    /*
+                     Set if you want to check the authentication status
+                     at the start up of the app
+                     */
+
+                    status: false,
+
+                    /*
+                     Enable cookies to allow the server to access
+                     the session
+                     */
+
+                    cookie: true,
+
+                    /* Parse XFBML */
+
+                    xfbml: true
+                });
+
+//                sAuth.watchAuthenticationStatusChange();
+
+            };
+
+            (function(d){
+                // load the Facebook javascript SDK
+
+                var js,
+                    id = 'facebook-jssdk',
+                    ref = d.getElementsByTagName('script')[0];
+
+                if (d.getElementById(id)) {
+                    return;
+                }
+
+                js = d.createElement('script');
+                js.id = id;
+                js.async = true;
+                js.src = "//connect.facebook.net/en_US/all.js";
+
+                ref.parentNode.insertBefore(js, ref);
+
+            }(document));
+
+        }]);
+
+    watchLoginChange = function() {
+
+        var _self = this;
+
+        FB.Event.subscribe('auth.authResponseChange', function(res) {
+
+            if (res.status === 'connected') {
+
+                /*
+                 The user is already logged,
+                 is possible retrieve his personal info
+                 */
+                _self.getUserInfo();
+
+                /*
+                 This is also the point where you should create a
+                 session for the current user.
+                 For this purpose you can use the data inside the
+                 res.authResponse object.
+                 */
+
+            }
+            else {
+
+                /*
+                 The user is not logged to the app, or into Facebook:
+                 destroy the session on the server.
+                 */
+
+            }
+
+        });
+
+    }
+    getUserInfo = function() {
+
+        var _self = this;
+
+        FB.api('/me', function(res) {
+            $rootScope.$apply(function() {
+                $rootScope.user = _self.user = res;
+            });
+        });
+
+    }
+    logout = function() {
+
+        var _self = this;
+
+        FB.logout(function(response) {
+            $rootScope.$apply(function() {
+                $rootScope.user = _self.user = {};
+            });
+        });
+
+    }
+
+    //--
 })();
 
