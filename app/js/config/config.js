002f10ce7aed90fbfbd7106c3c06f8cb894c14c5
sebalr <larrieu.sebastian@gmail.com>
2016-06-09 22:45:15 -0300

    token

---
+++ app/js/config/config.js
@@ -0,0 +1,9 @@
+/**
+ * Created by sebalr on 09/06/16.
+ */
+module.exports = {
+
+    'secret': 'iaw2016proyecto3',
+    'database': 'mongodb://localhost/DB_PELICULAS'
+
+};
--- server.js
+++ server.js
@@ -4,13 +4,19 @@ var express = require("express"),
     methodOverride = require("method-override");
     mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/DB_PELICULAS', function(err, res) {
+var jwt    = require('jsonwebtoken');
+var config = require('./app/js/config/config');
+
+mongoose.connect(config.database, function(err, res) {
     if(err) {
         console.log('ERROR: connecting to Database. ' + err);
     }else{
         console.log('conecte');
     }
 });
+
+
+app.set('claveSecreta', config.secret);
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -26,12 +32,9 @@ app.use(function (req,res,next) {
 // Import Models and controllers
 var models     = require('./app/models/movies')(app, mongoose);
 var moviesCtrl = require('./app/controllers/movies');
+var User   = require('./app/models/user');
 
-// Example Route
 var router = express.Router();
-router.get('/', function(req, res) {
-    res.send("Hello world!");
-});
 
 app.use(router);
 
@@ -51,4 +54,100 @@ app.use('/api', movies);
 
 app.listen(3000, function() {
     console.log("Node server running on http://localhost:3000");
+});
+
+                                            /*AUTENTICACION*/
+app.get('/setup', function(req, res) {
+
+    // create a sample user
+    var nick = new User({
+        name: 'Nick Rivera',
+        password: 'imDoctor',
+        admin: true
+    });
+
+    // save the sample user
+    nick.save(function(err) {
+        if (err) throw err;
+
+        console.log('User saved successfully');
+        res.json({ success: true });
+    });
+});
+
+// API AUTENTICATION ROUTES -------------------
+
+// route to authenticate a user (POST http://localhost:3000/api/authenticate)
+movies.post('/authenticate', function(req, res) {
+
+    // bucar usuario
+    User.findOne({
+        name: req.body.name
+    }, function(err, user) {
+
+        if (err) throw err;
+
+        if (!user) {
+            res.json({ success: false, message: 'Error de autenticacion, contraseña o usuario erroneo' });
+        } else if (user) {
+
+            // chequeo de contraseña
+            if (user.password != req.body.password) {
+                res.json({ success: false, message: 'Error de autenticacion, contraseña o usuario erroneo' });
+            } else {
+                //Creo el token
+                var token = jwt.sign(user, app.get('claveSecreta'), {
+                    expiresIn: 120 //Valido por 2 horas
+                });
+
+                //Retorno informcacion y token
+                res.json({
+                    success: true,
+                    message: 'Autenticacion correcta',
+                    token: token
+                });
+            }
+
+        }
+
+    });
+});
+
+
+// route middleware
+movies.use(function(req, res, next) {
+
+    //Busco el token en los parametros o header
+    var token = req.body.token || req.query.token || req.headers['x-access-token'];
+
+    //Decodifico el token
+    if (token) {
+        jwt.verify(token, app.get('claveSecreta'), function(err, decoded) {
+            if (err) {
+                return res.json({ success: false, message: 'No puedo autenticar el token, es trucho o expiro' });
+            } else {
+                req.decoded = decoded;
+                next();
+            }
+        });
+
+    } else {
+        return res.status(403).send({
+            success: false,
+            message: 'Falto el token'
+        });
+
+    }
+});
+
+// route to show a random message (GET http://localhost:8080/api/)
+movies.get('/index', function(req, res) {
+    res.json({ message: 'Welcome to the coolest API on earth!' });
+});
+
+// route to return all users (GET http://localhost:8080/api/users)
+movies.get('/users', function(req, res) {
+    User.find({}, function(err, users) {
+        res.json(users);
+    });
 });
