2bc6b9de7722a7c3edfbff1666f8922ed663ee8d
aguskoll <aguskoll@gmail.com>
2016-06-17 16:54:31 -0300

    palabras clave un poco mejorado

--- app/backend/controllers/movies.js
+++ app/backend/controllers/movies.js
@@ -26,13 +26,13 @@ exports.buscarRelacionadas=function(req, res) {
     Movie.find(function (err, movies) {
             if (err) return res.send(500, err.message);
             var salida = new Array();
-
+            var suficiente=false;
         if(pelicula!=null) {
             var palabras = pelicula.referencias.toString().split(' ');
             var i = 0;
 
             var puntero = 0;
-            for (i; i < movies.length; i++) {
+            for (i; i < movies.length&&!suficiente; i++) {
 
                 var j = 0;
                 var relaciona = false;
@@ -42,7 +42,7 @@ exports.buscarRelacionadas=function(req, res) {
                         var k = 0;
 
                         for (k; k < palabrasMovie.length && !relaciona; k++) {
-                            if (palabras[j] == palabrasMovie[k]) {
+                            if (palabras[j] == palabrasMovie[k]&&palabras[j]!=' ') {
                                 salida[puntero] = movies[i];
                                 puntero++;
                                 relaciona = true;
@@ -51,6 +51,8 @@ exports.buscarRelacionadas=function(req, res) {
                         }
                     }
                 }
+                if(salida.length>3)
+                    suficiente=true;
             }
             console.log('Relacionadas /movies');
         }
